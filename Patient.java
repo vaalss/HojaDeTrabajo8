e711c23f57b2676ead48962abf85b37eea130747
vaalss <valeria.hernandezmaldo@gmail.com>
2026-05-20 17:01:36 -0600

    clase paciente e interfaz cola de prioridad

---
+++ Patient.java
@@ -0,0 +1,34 @@
+public class Patient implements Comparable<Patient>{
+    private String name;
+    private String symptom;
+    private char priority;
+
+    public Patient(String name, String symptom, char priority) {
+        this.name = name;
+        this.symptom = symptom;
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSymptom() {
+        return this.symptom;
+    }
+
+    public char getPriority() {
+        return this.priority;
+    }
+
+    @Override
+    public int compareTo(Patient other) {
+        return this.priority - other.priority; //comparar los caracteres ASCII, A < B (A tiene mayor prioridad)
+    }
+
+    @Override
+    public String toString() {
+        return name + ", " + symptom + ", " + ": " + priority;
+    }
+
+}
---
+++ PriorityQueue.java
@@ -0,0 +1,7 @@
+public interface PriorityQueue<E extends Comparable<E>>{
+    void add(E value);
+    E remove();
+    E getFirst();
+    boolean isEmpty();
+    int size();
+}
