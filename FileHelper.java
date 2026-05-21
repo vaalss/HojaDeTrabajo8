79d4a141171d46835ca7c181105e8ce882ff617e
vaalss <valeria.hernandezmaldo@gmail.com>
2026-05-20 18:04:59 -0600

    lectura de archivo

---
+++ FileHelper.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class FileHelper {
+    public static ArrayList<Patient> loadPatients(String fileName) {
+        ArrayList<Patient> patients = new ArrayList<>();
+
+        try {
+            File file = new File(fileName);
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(",");
+
+                String name = parts[0].trim();
+                String symptom = parts[1].trim();
+                char priority = parts[2].trim().charAt(0);
+
+                Patient patient = new Patient(name, symptom, priority);
+                patients.add(patient);
+
+            }
+
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Archivo no encontrado");
+        }
+
+        return patients;
+    }
+}
--- Main.java
+++ Main.java
@@ -1,18 +1,20 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
         VectorHeap<Patient> cola = new VectorHeap<>();
 
-        cola.add(new Patient("Juan","Fractura de pierna",'C'));
+        ArrayList<Patient> pacientes =
+                FileHelper.loadPatients("pacientes.txt");
 
-        cola.add(new Patient("Maria","Apendicitis",'A'));
-
-        cola.add(new Patient("Josúe","Sarampión",'E'));
-
-        cola.add(new Patient("Carmen","Dolores de parto", 'B'));
+        for (Patient paciente : pacientes) {
+            cola.add(paciente);
+        }
 
         System.out.println("Orden de atención:");
+
         while (!cola.isEmpty()) {
             System.out.println(cola.remove());
         }
